5bd790ee64d83999ad9535d438154e372c6a319c
musin2 <muhsindaud75@gmail.com>
2024-09-13 18:14:36 +0300

    adds lab solution

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,41 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self,name, pet_type, owner = None) -> None:
+        self.name = name
+        self.pet_type = pet_type 
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, val):
+        if val not in Pet.PET_TYPES:
+            raise ValueError('Pet type not found in our system, the pet type must include {}'.format(','.join(Pet.PET_TYPES)))
+        else:
+            self._pet_type = val
+
 
 class Owner:
-    pass
+    
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def return_all_owner_pets(self):
+        return [{'name': pet.name, 'type': pet.type} for pet in Pet.all if pet.owner == self]
+    
+    def pets(self):
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise ValueError("pet must be an instance of class Pet")
+        
+    def get_sorted_pets(self):
+        sorted_list = sorted(Pet.all, key = lambda p: p.name)
+        return sorted_list
---
+++ lib/test.py
@@ -0,0 +1,5 @@
+from owner_pet import Pet
+
+pet1 = Pet("Fido", 'Dog')
+
+print(pet1.pet_type)
